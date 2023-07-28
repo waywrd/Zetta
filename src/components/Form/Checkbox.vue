94c135840097b95e163a524168369f0e1c48b9b5
waywrd <hdimitrov1511@gmail.com>
2023-07-28 14:34:01 +0300

    wip-2

---
+++ src/components/Form/Checkbox.vue
@@ -0,0 +1,85 @@
+<script setup>
+import { ref } from 'vue';
+const error = defineProps({ checkboxError: {} })
+const emit = defineEmits(['addPreference'])
+
+
+const contactOptions = ref(['Email', 'Phone', 'Call', 'SMS'])
+
+function changePreference(value) {
+    console.log(value)
+    emit('changePreference', value)
+
+}
+
+
+
+</script>
+<template>
+    <label class='checkbox-label'>Contact Preferences</label>
+    <div class='checkbox-container'>
+        <label class='contact-label' v-for="( option, index ) in  contactOptions " :key="index">
+            <input @change='changePreference(option)' type="checkbox" :value="option" />
+            <span class='checkmark'></span>
+            {{ option }}
+        </label>
+    </div>
+    <div class='error-message' v-if="checkboxError.state">{{ checkboxError.text }}</div>
+</template>
+<style>
+.checkbox-container {
+    display: flex;
+    justify-content: space-around;
+}
+.checkbox-label {
+    align-self: flex-start;
+    font-size: 13px;
+    padding-bottom: 1px;
+}
+.contact-label {
+    position: relative;
+    cursor: pointer;
+    font-size: 17px;
+    padding-left: 25px;
+}
+input[type='checkbox'] {
+    opacity: 0;
+    position: absolute;
+    cursor: pointer;
+}
+.contact-label input:checked~.checkmark {
+    background-color: white;
+}
+.checkmark {
+    position: absolute;
+    top: 3px;
+    left: 4px;
+    height: 15px;
+    width: 15px;
+    border: 1px solid #555;
+    color: black
+}
+.checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+}
+.contact-label input:checked~.checkmark:after {
+    display: block;
+}
+.contact-label .checkmark:after {
+    left: 3px;
+    bottom: 1px;
+    width: 7px;
+    height: 14px;
+    border: solid black;
+    border-width: 0 2px 2px 0;
+    transform: rotate(40deg);
+}
+.error-message {
+    align-self: flex-start;
+    color: red;
+    font-size: 13px;
+    padding-top: 1px;
+}
+</style>
---
+++ src/components/Table/Export.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({ tableData: [] })
+
+function openDialog() {
+    let dialog = document.getElementById('modal')
+    dialog.showModal()
+}
+function closeDialog() {
+    let dialog = document.getElementById('modal')
+    dialog.close()
+}
+
+
+</script>
+<template>
+    <button @click='openDialog()'>Export</button>
+    <dialog id='modal'>
+        <div class='modal-content'>
+            <span @click='closeDialog()' class='modal-close'>X</span>
+
+            <pre>{{ JSON.stringify(tableData, null, 4) }}</pre>
+        </div>
+    </dialog>
+</template>
+<style scoped>
+#data-dialog {
+    position: relative
+}
+.modal-close {
+    position: absolute;
+    top: 10px;
+    right: 15px;
+    cursor: pointer;
+}
+</style>
